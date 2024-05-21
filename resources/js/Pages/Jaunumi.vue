93ea0f6f08ad283f897df691a51e1db22603bd0c
Valerio1456 <davidnecaev2@gmail.com>
2024-05-21 12:09:49 +0300

    Update jaunumi

--- resources/js/Pages/Jaunumi.vue
+++ resources/js/Pages/Jaunumi.vue
@@ -9,13 +9,155 @@ import { Head } from '@inertiajs/vue3';
     <Welcome>
         
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Jaunumi</div>
-                </div>
-            </div>
-        </div>
+    
+<section class="mt-6 py-20">
+<div class="mx-auto grid max-w-2xl items-start gap-x-16 gap-y-8 px-6 lg:max-w-7xl lg:grid-cols-2">
+ <div class="group relative" preload="preload">
+ <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/whatsnewphp84.png" alt="A Look at What&#039;s Coming to PHP 8.4 image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<div class="mt-6 flex items-center gap-3">
+<span class="inline-flex rounded-full px-4 py-2 text-xs font-bold leading-4 border border-red-600 text-red-600">
+News
+</span>
+
+</div>
+
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+A Look at What&#039;s Coming to PHP 8.4
+</h3>
+
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/php-8-4-0" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<!--[if ENDBLOCK]><![endif]-->
+<!--[if BLOCK]><![endif]--> <div class="group relative" preload="preload">
+ <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/forge2.png" alt="Laravel Forge image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <div class="mt-6 flex items-center gap-3">
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if BLOCK]><![endif]--> <!--[if BLOCK]><![endif]--> <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 inline-flex !rounded-full px-4 py-2 text-xs font-bold leading-4 hover:opacity-80 focus-visible:ring-offset-1 border border-red-600 text-red-600" href="/advertising" wire:navigate.hover>
+Partner
+</a>
+<!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+</div>
+<!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+Laravel Forge - The easiest way to deploy your Laravel applications
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://forge.laravel.com/?utm_campaign=laravel&amp;utm_source=laravelnews" onclick="fathom.trackGoal(&#039;W6ZXNYBA&#039;, 0);">
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<!--[if ENDBLOCK]><![endif]-->
+</div>
+</section>
+<div class="mx-auto max-w-2xl px-6 lg:max-w-7xl">
+<hr class="border-gray-600/30" />
+</div>
+<section wire:snapshot="{&quot;data&quot;:{&quot;except&quot;:[[3572],{&quot;s&quot;:&quot;arr&quot;}],&quot;limit&quot;:6},&quot;memo&quot;:{&quot;id&quot;:&quot;fzmChqc4MQ61ls8FKAEm&quot;,&quot;name&quot;:&quot;latest-articles-section&quot;,&quot;path&quot;:&quot;\/&quot;,&quot;method&quot;:&quot;GET&quot;,&quot;children&quot;:[],&quot;scripts&quot;:[],&quot;assets&quot;:[],&quot;errors&quot;:[],&quot;locale&quot;:&quot;en&quot;},&quot;checksum&quot;:&quot;419c7e37a300f3f4f579682554b03326b77f69f07e9c0b3dbba35e20732af454&quot;}" wire:effects="[]" wire:id="fzmChqc4MQ61ls8FKAEm" class="py-20">
+<div class="mx-auto w-full max-w-2xl px-6 lg:max-w-7xl">
+<div class="flex flex-wrap items-center justify-between gap-x-8 gap-y-3">
+<h2 class="text-3xl font-bold sm:text-4xl lg:text-[40px]">The latest</h2>
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 font-bold text-red-600 hover:text-red-700" href="/blog" wire:navigate.hover>
+View all &rarr;
+</a>
+</div>
+<div class="mt-12 grid gap-x-8 gap-y-12 lg:grid-cols-3">
+<!--[if BLOCK]><![endif]--> <div class="group relative" wire:key="latest-laracon-au-2024-tickets-on-sale">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/439998172_3628342590813429_2269455672136702991_n.png" alt="Laracon AU 2024 tickets are now on sale image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+Laracon AU 2024 tickets are now on sale
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/laracon-au-2024-tickets-on-sale" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<div class="group relative" wire:key="latest-dash-ui-blade-component-library">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/dash-ui.png" alt="Dash UI is a Laravel Blade Component Library Inspired by Shopify Polaris image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+Dash UI is a Laravel Blade Component Library Inspired by Shopify Polaris
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/dash-ui-blade-component-library" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<div class="group relative" wire:key="latest-php-8-4-0">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/whatsnewphp84.png" alt="A Look at What&#039;s Coming to PHP 8.4 image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+A Look at What&#039;s Coming to PHP 8.4
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/php-8-4-0" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<div class="group relative" wire:key="latest-php-8-4-class-instantiation-without-extra-parenthesis">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/php-8-4-class-instantiation-without-extra-parenthesis.png" alt="Is class instantiation without extra parenthesis coming to PHP 8.4? image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+Is class instantiation without extra parenthesis coming to PHP 8.4?
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/php-8-4-class-instantiation-without-extra-parenthesis" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<div class="group relative" wire:key="latest-validation-errors-card-for-laravel-pulse">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/pulse-validation-errors-card-featured.png" alt="Validation Errors Card for Laravel Pulse image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+Validation Errors Card for Laravel Pulse
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/validation-errors-card-for-laravel-pulse" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<div class="group relative" wire:key="latest-frankenphp-octane-out-of-beta">
+<!--[if BLOCK]><![endif]--> <div class="aspect-[2/1] w-full rounded-lg bg-gray-100 shadow-card transition group-hover:opacity-80">
+<img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/featured-images/frankenphp-featured.png" alt="FrankenPHP Support in Laravel Octane is Out of Beta image" class="h-full w-full rounded-lg object-cover" loading="lazy" />
+</div>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<h3 class="mt-4 text-xl font-bold transition group-hover:text-red-600 sm:text-2xl">
+FrankenPHP Support in Laravel Octane is Out of Beta
+</h3>
+<!--[if BLOCK]><![endif]--> <!--[if ENDBLOCK]><![endif]-->
+<!--[if ENDBLOCK]><![endif]-->
+<a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 absolute inset-0 !block h-full w-full !rounded-lg" href="https://laravel-news.com/frankenphp-octane-out-of-beta" onclick>
+<span class="sr-only">Read article</span>
+</a>
+</div>
+<!--[if ENDBLOCK]><![endif]-->
+</div>
+</div>
+</section>
     </Welcome>
 
     
