f8803f4857a1eb1a2ec821fd406359baeab371a2
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2024-05-21 12:08:59 +0300

    par mums

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,8 +14,8 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt- border-b-2 border-main text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-main transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-gray-300 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-second focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+        ? 'mt-1 inline-flex items-center px-1 border-t-4 border-main text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-main transition duration-150 ease-in-out'
+        : 'mt-1 inline-flex items-center px-1 border-t-4 border-gray-300 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-second focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
 );
 </script>
 
