78d74768fcbca14c2eb161a2fd4a2737be45e3b0
Jegors Kalejs <jegors.kalejs@gmail.com>
2024-05-22 21:59:32 +0300

    Vakances sort start2.0

--- resources/js/Pages/Vakances.vue
+++ resources/js/Pages/Vakances.vue
@@ -25,6 +25,94 @@ const notify = () => {
             <div class="mx-auto" >
                 <div class="mx-auto p-10 ml-5 mr-5 text-gray-900">
                     <div class="p-10 ml-5 mr-5 text-gray-900">
+
+                        <div class="flex flex-col lg:flex-row lg:items-center max-lg:gap-4 justify-between w-full">
+
+                            <div class="relative w-full max-w-sm mb-4 lg:mb-0">
+            <svg class="absolute top-1/2 -translate-y-1/2 left-4 z-50" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16.5555 3.33203H3.44463C2.46273 3.33203 1.66675 4.12802 1.66675 5.10991C1.66675 5.56785 1.84345 6.00813 2.16004 6.33901L6.83697 11.2271C6.97021 11.3664 7.03684 11.436 7.0974 11.5068C7.57207 12.062 7.85127 12.7576 7.89207 13.4869C7.89728 13.5799 7.89728 13.6763 7.89728 13.869V16.251C7.89728 17.6854 9.30176 18.6988 10.663 18.2466C11.5227 17.961 12.1029 17.157 12.1029 16.251V14.2772C12.1029 13.6825 12.1029 13.3852 12.1523 13.1015C12.2323 12.6415 12.4081 12.2035 12.6683 11.8158C12.8287 11.5767 13.0342 11.3619 13.4454 10.9322L17.8401 6.33901C18.1567 6.00813 18.3334 5.56785 18.3334 5.10991C18.3334 4.12802 17.5374 3.33203 16.5555 3.33203Z" stroke="black" stroke-width="1.6" stroke-linecap="round" />
+            </svg>
+            <select id="Offer" class="h-12 border border-gray-300 text-gray-900 pl-11 text-base font-normal leading-7 rounded-full block w-full py-2.5 px-4 appearance-none relative focus:outline-none bg-white transition-all duration-500 hover:border-gray-400 hover:bg-gray-50 focus-within:bg-gray-50">
+                <option selected>Sort by time (high to low)</option>
+                <option value="option 1">option 1</option>
+                <option value="option 2">option 2</option>
+                <option value="option 3">option 3</option>
+                <option value="option 4">option 4</option>
+            </select>
+        </div>
+
+        <!-- Your Workspace Section -->
+        <div class="box rounded-xl border border-gray-300 bg-white p-6 w-full max-w-sm mb-4 lg:mb-0">
+            <h6 class="font-medium text-base leading-7 text-black mb-5">Your Workspace</h6>
+            <div class="flex items-center mb-5 gap-1">
+                <div class="relative w-full">
+                    <select id="FROM" class="h-12 border border-gray-300 text-gray-900 text-xs font-medium rounded-full block w-full py-2.5 px-4 appearance-none relative focus:outline-none bg-white">
+                        <option selected>Min</option>
+                        <option value="option 1">option 1</option>
+                <p class="px-1 font-normal text-sm leading-6 text-gray-600">to</p>
+                <div class="relative w-full">
+                    <select id="TO" class="h-12 border border-gray-300 text-gray-900 text-xs font-medium rounded-full block w-full py-2.5 px-4 appearance-none relative focus:outline-none bg-white">
+                        <option selected>Max</option>
+                        <option value="option 1">option 1</option>
+                        <option value="option 2">option 2</option>
+                        <option value="option 3">option 3</option>
+                        <option value="option 4">option 4</option>
+                    </select>
+                </div>
+            </div>
+
+            <label for="zip-code" class="block mb-2 text-sm font-medium text-gray-600 w-full">Zip Code</label>
+            <div class="relative w-full mb-8">
+                <select id="zip-code" class="h-12 border border-gray-300 text-gray-900 text-xs font-medium rounded-full block w-full py-2.5 px-4 appearance-none relative focus:outline-none bg-white">
+                    <option selected>Write code</option>
+                    <option value="option 1">option 1</option>
+                    <option value="option 2">option 2</option>
+                    <option value="option 3">option 3</option>
+                    <option value="option 4">option 4</option>
+                </select>
+            </div>
+
+            <button class="w-full py-2.5 flex items-center justify-center gap-2 rounded-full bg-indigo-600 text-white font-semibold text-xs shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700 hover:shadow-indigo-200">
+                <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.4987 13.9997L13.1654 12.6663M13.832 7.33301C13.832 10.6467 11.1457 13.333 7.83203 13.333C4.51832 13.333 1.83203 10.6467 1.83203 7.33301C1.83203 4.0193 4.51832 1.33301 7.83203 1.33301C11.1457 1.33301 13.832 4.0193 13.832 7.33301Z" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                Search
+            </button>
+        </div>
+
+        <!-- Search Section -->
+        <div class="relative w-full max-w-sm">
+            <input type="text" id="search" class="h-12 border border-gray-300 text-gray-900 pl-4 text-base font-normal leading-7 rounded-full block w-full py-2.5 px-4 appearance-none relative focus:outline-none bg-white transition-all duration-500 hover:border-gray-400 hover:bg-gray-50 focus-within:bg-gray-50" placeholder="Vakanču meklēšana">
+            <button class="absolute top-0 right-0 h-12 px-4 bg-green-500 text-white rounded-r-full focus:outline-none hover:bg-green-600">Meklēt</button>
+        </div>
+                        </div>
+                </div>
+            
+
+   
+             
+                        
+
+
+       
+                                            
+
+
+
+                        <form class="max-w-lg mx-auto mb-10">   
+                            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                            <div class="relative">
+                                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    </svg>
+                                </div>
+                                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-main focus:border-main " placeholder="Vakanču meklēšana" required />
+                                <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-main hover:bg-accent  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 transition-all ">Meklēt</button>
+                            </div>
+                        </form>
+
+
 <!-- вакансии -->
 
                         <!-- Выравнивание всех контейнеров -->
