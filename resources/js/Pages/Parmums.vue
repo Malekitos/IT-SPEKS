6c95e5cce567572f1f0362a9e2a45af3943792af
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2024-05-29 10:06:37 +0300

    responsive

--- resources/js/Pages/Parmums.vue
+++ resources/js/Pages/Parmums.vue
@@ -10,15 +10,17 @@ import FooterAll from '@/Pages/FooterAll.vue';
     <Welcome>
         
         <div class="relative flex justify-center">
-            <img src="../../photos/background.jpeg" class="blur-sm brightness-50 w-full h-auto">
+            <img src="../../photos/background.jpeg" class="blur-sm brightness-50 w-full h-auto object-cover max-[600px]:hidden">
             <div class="absolute">
-                <h1 class="font-bold text-white text-8xl mt-40">IT-SPEKS</h1>
+                <h1 class="font-bold text-white text-8xl mt-40 max-[600px]:bg-main max-[600px]:text-6xl max-[600px]:mt-14">IT-SPEKS</h1>
+                
             </div>
-            <div class="absolute mt-80 bg-white  max-w-4xl pb-40 rounded-sm text-center">
-                <h1 class="m-10 font-bold text-gray-800 text-4xl">IT-SPEKS programmatūras izstrādes uzņēmums jūsu vajadzībām ar ilgtermiņa atbalstu.</h1>
+            <div class="max-[600px]:mt-36 absolute mt-80 bg-white mx-5 pb-28 rounded-sm text-center max-[600px]:pb-7 max-[600px]:border-solid max-[600px]:border-main max-[600px]:border-2">
+                <h1 class="pt-7 font-bold text-gray-800 text-4xl max-[600px]:text-3xl">IT-SPEKS programmatūras izstrādes uzņēmums</h1>
+                <h1 class="font-bold text-gray-800 text-4xl max-[600px]:text-3xl">jūsu vajadzībām ar ilgtermiņa atbalstu.</h1>
                 <p class="m-10 text-slate-500 text-2xl">"Tehnoloģiju pasaulē nepārtraukta mācīšanās nav tikai stratēģija, tā ir izdzīvošana."</p>
 
-                <div class="flex justify-around mt-10 text-slate-600">
+                <div class="flex justify-around m-5 text-slate-600">
                     <div class="text-center">
                         <svg class="w-6 h-6 text-main dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.556 8.5h8m-8 3.5H12m7.111-7H4.89a.896.896 0 0 0-.629.256.868.868 0 0 0-.26.619v9.25c0 .232.094.455.26.619A.896.896 0 0 0 4.89 16H9l3 4 3-4h4.111a.896.896 0 0 0 .629-.256.868.868 0 0 0 .26-.619v-9.25a.868.868 0 0 0-.26-.619.896.896 0 0 0-.63-.256Z"/></svg>
