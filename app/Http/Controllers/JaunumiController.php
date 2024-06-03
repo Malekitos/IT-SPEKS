57b7bc77aea65441363110380c5450c3d9c0eb51
Jegors Kalejs <jegors.kalejs@gmail.com>
2024-06-03 19:47:57 +0300

    JaunumiController update

--- app/Http/Controllers/JaunumiController.php
+++ app/Http/Controllers/JaunumiController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Jaunumi;
 
 class JaunumiController extends Controller
 {
-    //
+    public function getJaunumi()
+    {
+        return response()->json(Jaunumi::latest()->get());
+    }
+
+    public function storeJaunumi(Request $request)
+    {
+            // Validacija 
+            $validated = $request->validate([
+                'nosaukums' => 'required|string|max:255',
+                'attels' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+    
+            // Saglaba bināra formāta
+            if ($request->hasFile('attels')) {
+                $image = $request->file('attels');
+                $imageData = file_get_contents($image->getRealPath()); //Binaras properties of img
+            } else {
+                return response()->json('Kļūda ar attēlu!');
+            }
+    
+            // New Jaunums
+            Jaunumi::create([
+                'nosaukums' => $validated['nosaukums'],
+                'attels' => $imageData,
+            ]);
+
+        return response()->json('Jaunums stored successfully!');
+    }
+
+    public function updateJaunums(Request $request, $id)
+    {
+        Jaunumi::where('id', $id)->update([
+            'nosaukums' => $request->nosaukums,
+            'attels' => $request->attels,
+        ]);
+
+        return response()->json('Jaunums updated successfully!');
+    }
+
+    public function removeJaunums($id)
+    {
+        Jaunumi::where('id', $id)->delete();
+        return response()->json('Jaunums removed successfully!');
+    }
+
 }
